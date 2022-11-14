df7637fffb4f913655885a2d4ed41c3cfbcd758a
ramyses-rodrigues <ramyses.rmr@gmail.com>
2022-11-14 18:53:24 -0300

    update

--- Form2.cs
+++ Form2.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using LibVLCSharp.Shared;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
-using Microsoft.VisualBasic.Devices;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace analise_libvlc
 {
